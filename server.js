045b93ddcf50b7cdd7317530588df6a5ed057170
blelian <bullsurus@gmail.com>
2026-06-29 13:17:12 +0200

    Done with week 2

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/organizations', async (req, res) => {
     const organizations = await getAllOrganizations();
       
     const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+    res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', async (req, res) => {
