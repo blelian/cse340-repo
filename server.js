4b7e501d5e22525b034a19ef9fd5b20a268bc0db
blelian <bullsurus@gmail.com>
2026-07-09 15:54:16 +0200

    done with the first part of flash messages and they work locally

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
 import router from './src/routes.js';
-
+import session from 'express-session';
+import flash from './src/middleware/flash.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -18,12 +19,25 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+const SESSION_SECRET = process.env.SESSION_SECRET;
+
 /**
   * Configure Express middleware
   */
 
 // Serve static files from the public directory
 // Allow Express to receive and process common POST data
+// Set up session management
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // Session expires after 1 hour of inactivity
+}));
+
+// Use flash message middleware
+app.use(flash);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
